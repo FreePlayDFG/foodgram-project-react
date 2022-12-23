2af80188f89569138c6c2cfe935a65e1ad1c2d73
Вячеслав Здобнов <reelay1@mail.ru>
2022-12-23 07:23:24 +0300

    18

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'SECRET_KEY')
 
 DEBUG = bool(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(', ')
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'users.User'
 
